fe2efe33876da9a0b6443b47766a48744de40188
Kalyan Burada <kalyanburada2006@gmail.com>
2026-02-22 01:38:34 +0530

    update strong password generator

--- lib/utils/password_generator.dart
+++ lib/utils/password_generator.dart
@@ -14,16 +14,50 @@ class PasswordGenerator {
     bool useSymbols = true,
   }) {
     if (length < 1) length = 1;
-    
-    String chars = '';
-    if (useLower) chars += _lower;
-    if (useUpper) chars += _upper;
-    if (useNumbers) chars += _numbers;
-    if (useSymbols) chars += _symbols;
 
-    if (chars.isEmpty) return '';
-
+    // Build character set and ensure at least one from each enabled category
     final rnd = Random.secure();
-    return List.generate(length, (_) => chars[rnd.nextInt(chars.length)]).join();
+    final requiredChars = <String>[];
+    String allChars = '';
+
+    // Add at least one character from each enabled category
+    if (useLower) {
+      allChars += _lower;
+      requiredChars.add(_lower[rnd.nextInt(_lower.length)]);
+    }
+    if (useUpper) {
+      allChars += _upper;
+      requiredChars.add(_upper[rnd.nextInt(_upper.length)]);
+    }
+    if (useNumbers) {
+      allChars += _numbers;
+      requiredChars.add(_numbers[rnd.nextInt(_numbers.length)]);
+    }
+    if (useSymbols) {
+      allChars += _symbols;
+      requiredChars.add(_symbols[rnd.nextInt(_symbols.length)]);
+    }
+
+    if (allChars.isEmpty) return '';
+
+    // If requested length is less than required characters,
+    // randomly select subset of required characters
+    if (length < requiredChars.length) {
+      requiredChars.shuffle(rnd);
+      return requiredChars.sublist(0, length).join();
+    }
+
+    // Build password with required characters + random fill
+    final password = <String>[...requiredChars];
+
+    // Fill the rest of the password length with random characters
+    while (password.length < length) {
+      password.add(allChars[rnd.nextInt(allChars.length)]);
+    }
+
+    // Shuffle to avoid predictable pattern (always having required chars at start)
+    password.shuffle(rnd);
+
+    return password.join();
   }
 }
